19487a62012c683e0c47f9700abb3add1cbb0e40
Abhiram9662 <abhiram23219@gmail.com>
2024-11-21 13:12:55 +0530

    Add files via upload

---
+++ Taxi/script.js
@@ -0,0 +1,8 @@
+document.getElementById('booking-form').addEventListener('submit', function (event) {
+  event.preventDefault();
+  alert('Thank you for booking with Aapne Taxi! Your ride has been scheduled.');
+});
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+  event.preventDefault();
+  alert('Thank you for reaching out! We will get back to you soon.');
+});
